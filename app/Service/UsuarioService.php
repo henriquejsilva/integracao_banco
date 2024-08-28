274567d0207023b4812d1437be8b4d50dc2488ab
henriquejsilva <henriquejosemartinsdasilva@gmail.com>
2024-08-28 15:38:44 -0300

    AULA DIA 28-08

--- app/Service/UsuarioService.php
+++ app/Service/UsuarioService.php
@@ -3,6 +3,7 @@
 namespace App\Service;
 
 use App\Models\Usuario;
+use Illuminate\Support\Facades\Redis;
 
 class UsuarioService
 {
@@ -16,24 +17,115 @@ class UsuarioService
          return $user;
      }
 
-     public function update(){
+     public function update(array $dados){
+         $usuario = Usuario::find($dados['id']);
 
+         if($usuario == null){
+            return [
+                'status' => false,
+                'message' => 'usuario não encontrado'
+            ];
+         }
+
+         if(isset($dados['password'])){
+         $usuario->password = $dados['password'];
+         }
+
+         if(isset($dados['nome'])){
+         $usuario->nome = $dados['nome'];
+         }
+
+         if(isset($dados['email'])){
+         $usuario->email = $dados['email'];
+         }
+         
+
+         $usuario->save(); 
+
+         return[
+            'status' => true,
+            'message' => 'Atualizado com sucesso'
+         ];
      }
 
-     public function delete(){
+     public function delete($id){
+         $usuario = Usuario::find($id);
 
+         if($usuario == null){
+            return [
+                'status' => false,
+                'message' => 'Usuario não encontrado'
+            ];
+         }
+
+         $usuario->delete();
+
+         return [
+            'status' => true,
+            'message' => "Usuario excluido com sucesso"
+         ];
      }
 
-     public function findById(){
+     public function findById($id){
+         $usuario = Usuario::find($id);
 
+         if($usuario == null){
+            return [
+                'status' => false,
+               'message' => 'Usuario não encontrado'
+            ];
+         }
+
+         return [
+            'status' => true,
+            'message' => 'Usuario encontrado',
+            'data' => $usuario
+         ];
      }
 
      public function getALL(){
+         $usuario = Usuario::all();
 
+         return [
+            'status' => true,
+            'message' => 'Pesquisa efetuada com sucesso',
+            'data' => $usuario
+         ];
      }
 
-     public function searchByName(){
+     public function searchByName($nome){
+        $usuarios = Usuario::where('nome', 'like', '%'. $nome . '%')->get();
 
+        if($usuarios->isEmpty()){
+            return[
+                'status' => false,
+                'message' => 'Sem resultados'
+            ];
+        }
+
+        return [
+            'status' => true,
+            'message' => 'Resultados Encontrados',
+            'data' => $usuarios
+        ];
+     }
+
+     public function searchByEmail($email){
+        $usuarios = Usuario::where('email', 'like', '%'. $email. '%')->get();
+
+        if($usuarios->isEmpty()){
+            return[
+                'status' => false,
+                'message' => 'Sem Resultado'
+               
+            ];
+        }
+
+        return [
+            'status' => true,
+            'message' => 'Resultado Encontrado',
+            'data' => $usuarios
+        ];
      }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/usuario', [UsuarioCrontroller::class, 'store']);
+
+Route::get('/usuario/{id}/find', [UsuarioCrontroller::class, 'findById'] );
+
+Route::get('/usuario', [UsuarioCrontroller::class, 'index']);
+
+Route::get('/usuario/search', [UsuarioCrontroller::class, 'searchByName']);
+
+Route::get('/usuario/search/email', [UsuarioCrontroller::class, 'searchByEmail']);
+
+Route::delete('/usuario/{id}/delete', [UsuarioCrontroller::class, 'delete']);
+
+Route::put('/usuario', [UsuarioCrontroller::class, 'update']);
